20f811dec87c3e98124aaf122c0723c160767ab7
Anass Hmadouch <102656228+AnassHmadouch@users.noreply.github.com>
2022-11-30 11:11:25 +0100

    Add files via upload

--- src/controleur/GestionAjoutLocataire.java
+++ src/controleur/GestionAjoutLocataire.java
@@ -4,26 +4,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JMenuItem;
 
 import vue.FenAjoutLocataire;
 
 public class GestionAjoutLocataire implements ActionListener{
-
-	private GestionMenu gestionClic;
-	private FenAjoutLocataire fenAjoutLocataire; 
+	
+	private FenAjoutLocataire fenAjoutLocataire;;
 	
 	public GestionAjoutLocataire(FenAjoutLocataire fenAjoutLocataire) {
 		this.fenAjoutLocataire = fenAjoutLocataire;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton item = (JButton) e.getSource();
 		switch(item.getText()) {
-		case "Annuler":
+		case "Ajouter":
 			this.fenAjoutLocataire.dispose();
 			break;
 		}
 	}
-
 }
