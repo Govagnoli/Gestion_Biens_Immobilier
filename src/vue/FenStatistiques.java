ba32cc2858de0407c2d9ba52eefc27c7aabf01af
LineCabaret <102655775+LineCabaret@users.noreply.github.com>
2022-12-05 08:27:43 +0100

    Add files via upload

--- src/vue/FenStatistiques.java
+++ src/vue/FenStatistiques.java
@@ -11,6 +11,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import controleur.GestionMenu;
 import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import java.awt.Canvas;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.Panel;
+import javax.swing.JScrollPane;
 
 public class FenStatistiques extends JFrame {
 
@@ -33,16 +45,98 @@ public class FenStatistiques extends JFrame {
 	public FenStatistiques() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		setUndecorated(false);
+		setBounds(100, 100, 1300, 600);
 		setVisible(true);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lbltest = new JLabel("Statistiques");
-		contentPane.add(lbltest, BorderLayout.CENTER);
+		JPanel general = new JPanel();
+		general.setBounds(34, 11, 674, 461);
+		contentPane.add(general);
+		general.setLayout(new BorderLayout(0, 0));
+		
+		JPanel Centre = new JPanel();
+		Centre.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		general.add(Centre, BorderLayout.CENTER);
+		Centre.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblTest = new JLabel("GRAPHIQUE");
+		lblTest.setHorizontalAlignment(SwingConstants.CENTER);
+		Centre.add(lblTest);
+		
+		JPanel Droit = new JPanel();
+		Droit.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		general.add(Droit, BorderLayout.EAST);
+		Droit.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblTest2 = new JLabel("GRAPHIQUE");
+		lblTest2.setHorizontalAlignment(SwingConstants.CENTER);
+		Droit.add(lblTest2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		general.add(scrollPane, BorderLayout.NORTH);
+		
+		JPanel block = new JPanel();
+		scrollPane.setViewportView(block);
+		GridBagLayout gbl_block = new GridBagLayout();
+		gbl_block.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_block.rowHeights = new int[] {0, 0, 0, 0};
+		gbl_block.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_block.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0};
+		block.setLayout(gbl_block);
+		
+		JPanel block1 = new JPanel();
+		scrollPane.setViewportView(block);
+		GridBagLayout gbl_block1 = new GridBagLayout();
+		gbl_block.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_block.rowHeights = new int[] {0, 0, 0, 0};
+		gbl_block.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_block.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0};
+		block.setLayout(gbl_block);
+		
+		JLabel lblTitre1 = new JLabel("Titre");
+		GridBagConstraints gbc_lblTitre1 = new GridBagConstraints();
+		gbc_lblTitre1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTitre1.gridx = 1;
+		gbc_lblTitre1.gridy = 0;
+		block.add(lblTitre1, gbc_lblTitre1);
+		
+		JLabel lblAdresse1 = new JLabel("Adresse du bien");
+		GridBagConstraints gbc_lblAdresse1 = new GridBagConstraints();
+		gbc_lblAdresse1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAdresse1.gridx = 0;
+		gbc_lblAdresse1.gridy = 1;
+		block.add(lblAdresse1, gbc_lblAdresse1);
+		
+		JLabel lblNbLoc1 = new JLabel("Nb locataire");
+		GridBagConstraints gbc_lblNbLoc1 = new GridBagConstraints();
+		gbc_lblNbLoc1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNbLoc1.gridx = 2;
+		gbc_lblNbLoc1.gridy = 1;
+		block.add(lblNbLoc1, gbc_lblNbLoc1);
+		
+		JLabel lblMontantTotal1 = new JLabel("Montant rapporté");
+		GridBagConstraints gbc_lblMontantTotal1 = new GridBagConstraints();
+		gbc_lblMontantTotal1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMontantTotal1.gridx = 1;
+		gbc_lblMontantTotal1.gridy = 2;
+		block.add(lblMontantTotal1, gbc_lblMontantTotal1);
+		
+		JLabel lblNbMois1 = new JLabel("Nb mois de location");
+		GridBagConstraints gbc_lblNbMois1 = new GridBagConstraints();
+		gbc_lblNbMois1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNbMois1.gridx = 0;
+		gbc_lblNbMois1.gridy = 3;
+		block.add(lblNbMois1, gbc_lblNbMois1);
+		
+		JLabel lblRevenu1 = new JLabel("Revenu/mois");
+		GridBagConstraints gbc_lblRevenu1 = new GridBagConstraints();
+		gbc_lblRevenu1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblRevenu1.gridx = 2;
+		gbc_lblRevenu1.gridy = 3;
+		block.add(lblRevenu1, gbc_lblRevenu1);
 		
 		this.gestionMenu = new GestionMenu(this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -50,22 +144,17 @@ public class FenStatistiques extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu informations = new JMenu("Informations");
-		menuBar.add(informations);
+		JMenu accueil1 = new JMenu("Accueil");
+		menuBar.add(accueil1);
 		
 		JMenu gestion = new JMenu("Gestion");
 		menuBar.add(gestion);
+		
+		JMenu parametres = new JMenu("Param\u00E8tres");
+		menuBar.add(parametres);
 
-		JMenu deconnexion = new JMenu("Fermer");
-		menuBar.add(deconnexion);
-		
-		JMenuItem statistiques = new JMenuItem("Statistiques");
-		statistiques.addActionListener(this.gestionMenu);
-		informations.add(statistiques);
-		
-		JMenuItem monCompte = new JMenuItem("Mon compte");
-		monCompte.addActionListener(this.gestionMenu);
-		informations.add(monCompte);
+		JMenu compte = new JMenu("Compte");
+		menuBar.add(compte);
 		
 		JMenuItem biens = new JMenuItem("Mes Biens Immobilier");
 		biens.addActionListener(this.gestionMenu);
@@ -74,10 +163,21 @@ public class FenStatistiques extends JFrame {
 		JMenuItem locataires = new JMenuItem("Locataires");
 		locataires.addActionListener(this.gestionMenu);
 		gestion.add(locataires);
-
+		
+		JMenuItem accueil = new JMenuItem("Accueil");
+		accueil.addActionListener(this.gestionMenu);
+		accueil1.add(accueil);
+				
+		JMenuItem statistiques = new JMenuItem("Statistiques");
+		statistiques.addActionListener(this.gestionMenu);
+		parametres.add(statistiques);
+		
 		JMenuItem fermer = new JMenuItem("Fermer");
 		fermer.addActionListener(this.gestionMenu);
-		deconnexion.add(fermer);
-	}
+		parametres.add(fermer);
 
+		JMenuItem monCompte = new JMenuItem("Mon compte");
+		monCompte.addActionListener(this.gestionMenu);
+		compte.add(monCompte);
+	}
 }
