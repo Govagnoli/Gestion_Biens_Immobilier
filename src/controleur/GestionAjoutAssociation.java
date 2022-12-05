0b7682029e2227115f34dbe1a94714656138d78c
Eliott BENAIM <81430707+Govagnoli@users.noreply.github.com>
2022-12-05 11:00:49 +0100

    Update GestionAjoutAssociation.java

--- src/controleur/GestionAjoutAssociation.java
+++ src/controleur/GestionAjoutAssociation.java
@@ -2,17 +2,21 @@ package controleur;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.JButton;
 
 import vue.FenAjoutAssociation;
 
-public class GestionAjoutAssociation implements ActionListener{
+public class GestionAjoutAssociation implements ActionListener, ItemListener{
 	
 	private FenAjoutAssociation fenAjoutAssociation;
+	private boolean donationMensuel;
 	
 	public GestionAjoutAssociation(FenAjoutAssociation fenAjoutAssociation) {
 		this.fenAjoutAssociation = fenAjoutAssociation;
+		this.donationMensuel = true;
 	}
 
 	@Override
@@ -36,5 +40,14 @@ public class GestionAjoutAssociation implements ActionListener{
 			this.fenAjoutAssociation.dispose();
 			break;
 		}
+	}
+	
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		if(this.fenAjoutAssociation.getRdbtnMensuel().getSelectedObjects() == null) {
+			this.donationMensuel = false;
+		} else {
+			this.donationMensuel = true;
+		}		
 	}
 }
