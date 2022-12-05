4a8be907a57b925e2f0bac78a1b07c27b70dee6d
Eliott BENAIM <81430707+Govagnoli@users.noreply.github.com>
2022-12-05 12:19:24 +0100

    Update GestionQuittances.java

--- src/controleur/GestionQuittances.java
+++ src/controleur/GestionQuittances.java
@@ -1,15 +1,31 @@
 package controleur;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import vue.FenQuittances;
 
-public class GestionQuittances implements ListSelectionListener {
-private FenQuittances fenQuittance;
+public class GestionQuittances  implements ActionListener, ListSelectionListener {
+	
+	private FenQuittances fenQuittance;
 	
 	public GestionQuittances(FenQuittances fenQuittance) {
 		this.fenQuittance = fenQuittance;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton item = (JButton) e.getSource();		
+		switch(item.getText()) {
+		case "Les Quittances de Loyer":
+			FenQuittances fenQuittance = new FenQuittances();
+			fenQuittance.setVisible(true);
+			break;
+		}
 	}
 	
 	@Override
