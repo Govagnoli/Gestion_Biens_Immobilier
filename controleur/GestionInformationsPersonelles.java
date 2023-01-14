3550576820dd4f032dba7f035c9e3138beb9cc69
Eliott BENAIM <81430707+Govagnoli@users.noreply.github.com>
2023-01-14 17:27:54 +0100

    Add files via upload

--- controleur/GestionInformationsPersonelles.java
+++ controleur/GestionInformationsPersonelles.java
@@ -2,11 +2,18 @@ package controleur;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
+import dao.DaoAssociations;
+import modele.Association;
+import modele.Locataire;
 import vue.FenAjoutAssociation;
 import vue.FenInformationsPersonelles;
 import vue.FenTelecharger;
@@ -46,4 +53,29 @@ public class GestionInformationsPersonelles implements ActionListener, ListSelec
 			this.fen.getBtnModifierAssociation().setVisible(true);
 		}
 	}
+	
+	public void chargerTableauAssociation() {
+		try {
+			DaoAssociations daoAssociations = new DaoAssociations();
+			List<Association> associations = daoAssociations.findAll();
+			int i = 0;
+			for(Association association: associations) {
+				ecrireLigneTableAssociations(i, association);
+				i++;
+			}
+		} catch (SQLException e) {
+			System.out.println("Erreur : problème pour charger le tableau des associations");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void ecrireLigneTableAssociations(int numeroLigne, Association association) throws SQLException {
+		DefaultTableModel modeleTable = (DefaultTableModel) this.fen.getTableAssociations().getModel();
+		modeleTable.setValueAt(association.getLibelle(), numeroLigne, 0);
+		modeleTable.setValueAt(association.getSiren(), numeroLigne, 1);
+		modeleTable.setValueAt(association.getDateDonation(), numeroLigne, 2);
+		modeleTable.setValueAt(association.getMontantDonation(), numeroLigne, 3);
+	}
+	
 }
