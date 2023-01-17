6066f704c02e8721a7dc1661e3e650228de98cc7
LineCabaret <102655775+LineCabaret@users.noreply.github.com>
2023-01-17 17:04:57 +0100

    Add files via upload

--- src/controleur/GestionLocations.java
+++ src/controleur/GestionLocations.java
@@ -13,9 +13,7 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
 import JDBC.CictOracleDataSource;
-import modele.BienImmobilier;
 import modele.Location;
-import vue.FenAjoutBien;
 import vue.FenLocations;
 
 public class GestionLocations implements ActionListener, ListSelectionListener{
@@ -34,11 +32,10 @@ public class GestionLocations implements ActionListener, ListSelectionListener{
 			try {
 				CictOracleDataSource.creerAcces("BNL4208A", "$iutinfo");
 				Connection connexion = CictOracleDataSource.getConnexion();			
-				String reqSelectionLocations = 
-						"Select S3_LOCATIONS.NUMERO, S3_LOCATIONS.ADRESSE, S3_LOCATAIRE.NOM,"
-						+ "S3_LOCATAIRE.PRENOM, S3_LOCATIONS.DATE_DEBUT_CONTRAT"
-						+ "FROM S3_LOCATIONS, S3_LOUER, S3_LOCATAIRE"
-						+ "WHERE S3_LOCATIONS.id_location = S3_LOUER.id_location"
+				String reqSelectionLocations = "Select S3_LOCATION.ID_LOCATION, S3_LOCATION.NUMERO, "
+						+ "S3_LOCATION.ADRESSE, S3_LOCATAIRE.NOM, S3_LOCATAIRE.PRENOM, S3_LOCATION.DATE_DEBUT_CONTRAT "
+						+ "FROM S3_LOCATION, S3_LOUER, S3_LOCATAIRE "
+						+ "WHERE S3_LOCATION.id_location = S3_LOUER.id_location "
 						+ "AND S3_LOUER.id_locataire = S3_LOCATAIRE.id_locataire";
 				Statement st = connexion.createStatement();
 				ResultSet res = st.executeQuery(reqSelectionLocations);
@@ -51,6 +48,7 @@ public class GestionLocations implements ActionListener, ListSelectionListener{
 			} catch(SQLException exception) {
 				exception.printStackTrace();
 			}
+			break;
 		}
 	}
 	
@@ -63,18 +61,25 @@ public class GestionLocations implements ActionListener, ListSelectionListener{
 		} else {
 			DefaultTableModel modeleTableLocation = (DefaultTableModel) this.fenLocations.getTableLocation().getModel();
 			this.fenLocations.getDetailslocation().setVisible(true);
-			String adresse = modeleTableLocation.getValueAt(ligneSelectionnee, 0).toString();
+			String idLocation = modeleTableLocation.getValueAt(ligneSelectionnee, 0).toString();
 			Location location = null;
-			this.fenLocations.getDetailslocation().setVisible(true);
+			try {
+				location = new Location(Integer.parseInt(idLocation));
+				this.fenLocations.getDetailslocation().setLocation(location);
+				this.fenLocations.getDetailslocation().setVisible(true);
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 		}
 	}
 	
 	public void ecrireLigneTable(int numeroLigne, ResultSet res) throws SQLException {
 		DefaultTableModel modeleTable = this.fenLocations.getModelTableLocation();
-		modeleTable.setValueAt(res.getInt("NUMERO"), numeroLigne, 0);
-		modeleTable.setValueAt(res.getString("ADRESSE"), numeroLigne, 1);
-		modeleTable.setValueAt(res.getString("NOM"), numeroLigne, 2);
-		modeleTable.setValueAt(res.getString("PRENOM"), numeroLigne, 3);
-		modeleTable.setValueAt(res.getDate("DATE_DEBUT_CONTRAT"), numeroLigne, 4);
+		modeleTable.setValueAt(res.getInt("ID_LOCATION"), numeroLigne, 0);
+		modeleTable.setValueAt(res.getInt("NUMERO"), numeroLigne, 1);
+		modeleTable.setValueAt(res.getString("ADRESSE"), numeroLigne, 2);
+		modeleTable.setValueAt(res.getString("NOM"), numeroLigne, 3);
+		modeleTable.setValueAt(res.getString("PRENOM"), numeroLigne, 4);
+		modeleTable.setValueAt(res.getDate("DATE_DEBUT_CONTRAT"), numeroLigne, 5);
 	}
 }
